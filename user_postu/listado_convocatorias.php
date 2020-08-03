f091de79ba3cad2d582fad6ee34a42bcaf79e4c3
AbelRM <jhanmaquera@gmail.com>
2020-08-03 10:19:19 -0500

    nav

--- user_postu/listado_convocatorias.php
+++ user_postu/listado_convocatorias.php
@@ -48,44 +48,9 @@
       <!-- Main Content -->
       <div id="content">
         <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-                
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $fila['nombres']." ".$fila['ape_pat']; ?></span>
-                <img class="img-profile rounded-circle" src="img/user.png">
-              </a>
-              <!-- Dropdown - User Information -->
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Mi perfil
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Opciones
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Actividad
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Cerrar sesión
-                </a>
-              </div>
-            </li>
-
-          </ul>
-
-        </nav>
+        <?php
+            include_once 'nav.php';
+        ?>
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
@@ -103,18 +68,36 @@
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-              
-                  <?php
-                      $dni = $_GET['dni'];
-                      $idcon=$_GET['id'];
-
-                      echo $idcon;
-
-        
-                      ?>
-
+                <thead>
+                    <tr>
+                      <th>N° CONVOCATORIA</th>
+                      <th>TIPO</th>
+                      <th>DIRECCION EJECUTORA</th>
+                      <th>ACCIONES</th>
+                      
+                    </tr>
+                  </thead>
                   <tbody>
-                    
+                     <?php
+                      $dni = $_GET['dni'];
+                      
+                      $sql3 = "SELECT * FROM full_convocatoria";
+                      $query=mysqli_query($con, $sql3);
+                      while ($row= MySQLI_fetch_array($query))
+                      {
+                    ?>
+                      <tr>
+                        <td style="font-size: 16px;"><?php echo $row['num_con']."-".$row['anio_con'] ?></td>
+                        <td style="font-size: 14px;"><?php echo $row['tipo_con']?></td>
+                        <td style="font-size: 14px;"><?php echo $row['direccion_ejec']?></td>
+                        <td>
+                          <button type="button" class="btn btn-primary"><i class="fa fa-eye"></i> Ver</button>
+                        </td>
+                        
+                      </tr>
+                    <?php
+                      }
+                    ?>
                   </tbody>
                 </table>
               </div>
@@ -155,7 +138,7 @@
         <div class="modal-body">Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-          <a class="btn btn-primary" href="../index.php">Cerrar sesión</a>
+          <a class="btn btn-primary" href="procesos/cerrar_sesion.php">Cerrar sesión</a>
         </div>
       </div>
     </div>

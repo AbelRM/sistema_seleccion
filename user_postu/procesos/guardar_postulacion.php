5fdb40763109641f2459edb65d7b6549178eab4a
GiselaF13 <48686385+GiselaF13@users.noreply.github.com>
2020-08-26 11:15:54 -0500

    fff

--- user_postu/procesos/guardar_postulacion.php
+++ user_postu/procesos/guardar_postulacion.php
@@ -10,7 +10,7 @@ $boleta=$_POST['boleta'];
 // $fecha_postulacion=$_POST['fecha_postulacion'];
 $date = date('Y-m-d');
 
-$sql="INSERT INTO detalle_convocatoria (convocatoria_idcon,postulante_idpostulante,personal_req_idpersonal,boleta,fecha_postulacion) 
+$sql="INSERT INTO detalle_convocatoria (convocatoria_idcon, postulante_idpostulante, personal_req_idpersonal, boleta, fech_inscripcion) 
 VALUES ('".$idcon."','".$idpostulante."','".$personal_req."','".$boleta."','".$date."')";
 
 $result = MYSQLI_query($con, $sql); 

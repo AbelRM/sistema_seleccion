b434795225b9aefcf1fdf3805156319dbc67b6c9
GiselaF13 <48686385+GiselaF13@users.noreply.github.com>
2020-09-08 12:33:45 -0500

    cambios

--- user_postu/mispostulaciones.php
+++ user_postu/mispostulaciones.php
@@ -22,6 +22,11 @@
   <!-- Custom styles for this page -->
   <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
+  <script type="text/javascript" src="vendor/sweetalert/sweetalert2.min.js" ></script>
+  <script type="text/javascript" src="vendor/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="vendor/sweetalert/sweetalert2.min.css">
+  <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
 </head>
 
 <body id="page-top">
@@ -97,10 +102,12 @@
                         <td style="font-size: 14px;"><?php echo $row['cargo']?></td>
                         <td style="font-size: 14px;"><?php echo $row['boleta']?></td>
                         <td style="font-size: 14px;"><?php echo $row['fecha_postulacion']?></td>
+
                         <td>
-                          <button type="button" class="btn btn-primary"><i class="fa fa-eye"></i> Ver</button>
+                          <a href="ver_postulaciones.php?id=<?php echo $row['iddetalle_convocatoria']?>&dni=<?php echo $dato_desencriptado?>"><button type="button" class="btn btn-primary" id="editar" style="margin: 1px;"><i class="fa fa-eye"></i> Ver</button></a>
+                          <button type="button"  class="btn btn-primary delete_product"><i class="fa fa-eye"></i> Confirmar</button>
+                         
                         </td>
-                        
                       </tr>
                     <?php
                       } 
@@ -167,6 +174,76 @@
 
   <!-- Page level custom scripts -->
   <script src="js/demo/datatables-demo.js"></script>
+
+  <script type="text/javascript">
+        $(document).ready(function(){
+        
+        $(document).on('click', '.button1', function(){
+
+          swal({
+              title: "¿Deseas unirte al lado oscuro?",
+              text: "Este paso marcará el resto de tu vida...",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#DD6B55",
+              confirmButtonText: "¡Claro!",
+              cancelButtonText: "No, jamás",
+              closeOnConfirm: false,
+              closeOnCancel: false },
+
+              function(isConfirm){
+              if (isConfirm) {
+              swal("¡Hecho!",
+              "Ahora eres uno de los nuestros",
+              "success");
+              } else {
+              swal("¡Gallina!",
+              "Tu te lo pierdes...",
+              "error");
+              }
+              });     
+        });
+
+    })
+  </script>
+
+  <script>
+  $(document).ready(function(){
+	fetch();
+
+	$(document).on('click', '.delete_product', function(){
+		var id = $(this).data('id');
+
+		swal({
+		  	title: 'Desea confirmar postulacion?',
+		  	text: "No podrás modificar la postulacion!",
+		  	type: 'warning',
+		  	showCancelButton: true,
+		  	confirmButtonColor: '#3085d6',
+		  	cancelButtonColor: '#d33',
+		  	confirmButtonText: 'Confirmar!',
+		}).then((result) => {
+		  	if (result.value){
+		  		$.ajax({
+			   		url: 'api.php?action=delete',
+			    	type: 'POST',
+			       	data: 'id='+id,
+			       	dataType: 'json'
+			    })
+			    .done(function(response){
+			     	swal('Deleted!', response.message, response.status);
+					fetch();
+			    })
+			    .fail(function(){
+			     	swal('Oops...', 'Something went wrong with ajax !', 'error');
+			    });
+		  	}
+
+		})
+
+	});
+});
+  </script>
 
 </body>
 
